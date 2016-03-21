6cb9ce1493a70d29f6c0b9aaf3475d5583a3617b
Tom <thomas.oleary@hotmail.co.uk>
2016-03-21 15:31:38 +0000

    built addCoord(), getAllCoords() and getCoordsNear() with a hardcoded coordinate

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
+// ###################
+// ###################
+// Server config
+// ###################
+// ###################
+
 var restify = require('restify');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 
 var server = restify.createServer();
-//bucketlistapp database, appUsers and bucketLists are collections (tables ish)
+
+//init restify and enable logging (morgan)
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(morgan('dev')); // LOGGER
+
+//enable cross origin request sharing so client can talk to any domain (i.e. my server)
+server.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header( 'Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+//start up server
+server.listen(process.env.PORT || 8080, function() {
+  console.log("Server started at", process.env.PORT || 8080);
+});
+
+// ###################
+// ###################
+// Database config
+// ###################
+// ###################
+
 mongoose.connect('mongodb://localhost/CoordDB');
 var db = mongoose.connection;
 db.on('error', function (err) {
@@ -35,51 +66,67 @@ coordSchema.pre('save', function(next){
 });
 // Indexes this schema in 2dsphere format
 coordSchema.index({location: '2dsphere'});
+//new location model
+mongoose.model('Location', coordSchema)
+var Location = mongoose.model('Location');
 
-var Location = mongoose.model('Location', coordSchema);
 
-//add Location
-var postCoord = function(){
-  var newLocation = new Location({
-  uuid : '77gsfgs8s',
-  location : [65.234, 12.11234]
+// ###################
+// ###################
+// REST API
+// ###################
+// ###################
+
+//add Location function
+var postCoord = function(req,res,next){
+  var newCoordinates = req.params;
+  JSON.stringify(newCoordinates);
+  var newLocation = new Location(newCoordinates);
+
+  newLocation.save(newCoordinates, function (err, data) {
+    if (err) console.log(err);
+    else console.log('Saved : ', data );
+    res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8' });
+    res.end(JSON.stringify(data));
   });
+}
 
-  newLocation.save(function (err, data) {
-  if (err) console.log(err);
-  else console.log('Saved : ', data );
+//get All locations function
+var getAllCoords = function(req,res,next){
+  Location.find({}).exec(function (arr,data) {
+    console.log('Entries : ', data );
+    res.send(JSON.stringify(data));
   });
+}
 
+  // _id: db.ObjectId(req.params.location)
+//get locations near a given location
+var getCoordsNear = function(req,res,next){
+  Location.find({
+    location: {
+      $near : {
+        $geometry : {
+          coordinates : [50.716135, -1.986723]
+        },
+        $maxDistance : 8000
+      }
+    }
+  }).exec(function (arr,data) {
+    console.log('Entries : ', data );
+    res.send(JSON.stringify(data));
+  });
 }
 
 
 
+
 server.post('/addCoord', postCoord);
+server.get('/getAllCoords',getAllCoords);
+server.get('/getCoordsNear',getCoordsNear);
 
-//init restify and enable logging (morgan)
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
-server.use(morgan('dev')); // LOGGER
 
-//enable cross origin request sharing so client can talk to any domain (i.e. my server)
-server.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-File-Name, Content-Type, Cache-Control' );
-  next();
-});
-
-//start up server
-server.listen(process.env.PORT || 8080, function() {
-  console.log("Server started at", process.env.PORT || 8080);
-});
-
-//######################//
-//DB SETUP
-//######################//
-
-Schema = mongoose.Schema;
+// var databaseConfig = require('./config/database')(server, db);
+// var manageCoords = require('./coordinates/manageCoords')(server, db);
 
 
 //TODO get coordiantes within a radius using https://github.com/robert52/simple-geolocation/blob/master/controller.js
